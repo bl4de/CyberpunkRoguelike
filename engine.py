faa47af649be5ca749683ed7e0da7ea7c30f5d77
Rafal Janicki <bloorq@gmail.com>
2021-11-28 20:21:03 +0000

    Part 10. - Saving the game

--- engine.py
+++ engine.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import lzma
+import pickle
 from typing import TYPE_CHECKING
 
 from tcod.console import Console
@@ -12,7 +14,7 @@ from render_functions import render_bar, render_names_at_mouse_location
 if TYPE_CHECKING:
     from entity import Actor
     from game_map import GameMap
-    
+
 
 class Engine:
     game_map: GameMap
@@ -55,6 +57,12 @@ class Engine:
 
         render_names_at_mouse_location(
             console=console, x=21, y=44, engine=self)
+
+    def save_as(self, filename: str) -> None:
+        """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
+        with open(filename, "wb") as f:
+            f.write(save_data)
 
 
 CURSOR_Y_KEYS = {
